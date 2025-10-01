92e3cffd1a0b4208cf9f80e03d77f47c3a403735
Maximilian <scientificresearch309@gmail.com>
2025-10-01 23:02:11 +0200

    we see the problem by http.createServer => we see the Json in string in browser

--- src/server.ts
+++ src/server.ts
@@ -11,16 +11,15 @@ const port = 8000;
 const jobs = JSON.parse(fs.readFileSync('./src/data/jobs.json', 'utf-8'));
 console.log(jobs);
 
-(() => {
-  http
-    .createServer(async (req: IncomingMessage, res: ServerResponse) => {
-      const html = await generateMainContent(); // HTML bei jedem Request neu generieren
-      res.writeHead(200, { 'Content-Type': 'text/html' });
-      // res.write('info site');
-      res.write(html);
-      res.end();
-    })
-    .listen(port);
+http
+  .createServer((req: IncomingMessage, res: ServerResponse) => {
+    // const html = await generateMainContent(); // HTML bei jedem Request neu generieren
+    res.writeHead(200, { 'Content-Type': 'text/html' });
+    // res.write('info site');
+    // res.write(html);
+    res.write(JSON.stringify(jobs));
+    res.end();
+  })
+  .listen(port);
 
-  console.log(`listening on http://localhost:${port}`);
-})();
+console.log(`listening on http://localhost:${port}`);
