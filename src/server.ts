72ab02946423c00dfb84902adcc9a123c095ca6e
Maximilian <scientificresearch309@gmail.com>
2025-10-01 23:18:52 +0200

    app.get() is working from express

--- src/server.ts
+++ src/server.ts
@@ -1,25 +1,35 @@
 // import http from 'http';
 // import * as http from 'http';
 
-import http from 'http';
+// import http from 'http';
+import express from 'express';
 import fs from 'fs';
-import { IncomingMessage, ServerResponse } from 'http';
-import { generateMainContent } from './content';
+// import { IncomingMessage, ServerResponse } from 'http';
+// import { generateMainContent } from './content';
 
+const app = express();
 const port = 8000;
 
 const jobs = JSON.parse(fs.readFileSync('./src/data/jobs.json', 'utf-8'));
 console.log(jobs);
 
-http
-  .createServer((req: IncomingMessage, res: ServerResponse) => {
-    // const html = await generateMainContent(); // HTML bei jedem Request neu generieren
-    res.writeHead(200, { 'Content-Type': 'application/json' });
-    // res.write('info site');
-    // res.write(html);
-    res.write(JSON.stringify(jobs));
-    res.end();
-  })
-  .listen(port);
+// using http.createServer
+// http
+//   .createServer((req: IncomingMessage, res: ServerResponse) => {
+//     // const html = await generateMainContent(); // HTML bei jedem Request neu generieren
+//     res.writeHead(200, { 'Content-Type': 'application/json' });
+//     // res.write('info site');
+//     // res.write(html);
+//     res.write(JSON.stringify(jobs));
+//     res.end();
+//   })
+//   .listen(port);
 
-console.log(`listening on http://localhost:${port}`);
+// using express
+app.get('/', (req: express.Request, res: express.Response) => {
+  res.send('job site api');
+});
+
+app.listen(port, () => {
+  console.log(`listening on http://localhost:${port}`);
+});
